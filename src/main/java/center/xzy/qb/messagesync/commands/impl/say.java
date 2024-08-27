01daaa7f235ae9ba9cb543713e55f9e6dfa869fe
gingmzmzx <2417481092@qq.com>
2024-08-27 21:01:58 +0800

    feat: color in say message

--- src/main/java/center/xzy/qb/messagesync/commands/impl/say.java
+++ src/main/java/center/xzy/qb/messagesync/commands/impl/say.java
@@ -2,6 +2,7 @@ package center.xzy.qb.messagesync.commands.impl;
 
 import center.xzy.qb.messagesync.Main;
 import center.xzy.qb.messagesync.commands.ICommand;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -18,9 +19,9 @@ public class say extends ICommand {
             msg.append(arg).append(" ");
         }
         if (sender instanceof Player){
-            Main.instance.getServer().broadcastMessage("<" + sender.getName() + "> " + msg);
+            Main.instance.getServer().broadcastMessage(ChatColor.YELLOW + "<" + sender.getName() + "> " + ChatColor.RESET + msg);
         }else {
-            Main.instance.getServer().broadcastMessage(plugin.getConfig().getString("say-prefix") + " " + msg);
+            Main.instance.getServer().broadcastMessage(ChatColor.YELLOW + plugin.getConfig().getString("say-prefix") + " " + ChatColor.RESET + msg);
         }
         return true;
     }
